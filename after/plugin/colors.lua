1c480bb2b7f21d45d37200e13c2fee43abdbbf0b
Fran García <francisco.garcia@jobandtalent.com>
2023-01-25 17:16:50 +0100

    first commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,47 @@
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  -- vim.cmd.colorscheme(color)
+  require("tokyonight").setup({
+    style = "night",
+    transparent = true,
+    on_highlights = function(hl, c)
+      local prompt = "none"
+      hl.TelescopeNormal = {
+        bg = prompt,
+        fg = c.fg_dark
+      }
+      hl.TelescopeBorder = {
+        bg = prompt,
+        fg = c.fg_dark,
+      }
+      hl.TelescopePromptNormal = {
+        bg = prompt,
+      }
+      hl.TelescopePromptBorder = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePromptTitle = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePreviewTitle = {
+        bg = prompt,
+        fg = c.fg_dark,
+      }
+      hl.TelescopeResultsTitle = {
+        bg = prompt,
+        fg = c.fg_dark,
+      }
+    end,
+  })
+  -- vim.cmd[[colorscheme tokyonight]]
+  -- vim.cmd[[colorscheme dragon-energy]]
+
+  vim.cmd[[colorscheme everforest]]
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
